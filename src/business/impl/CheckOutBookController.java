cb0de3b40e65913811bcbc04e14c5027918c11a3
HasibRashid95 <hasibrashid95@github.com>
2022-06-30 14:28:25 -0500

    Implement CheckOutBookController

---
+++ src/business/impl/CheckOutBookController.java
@@ -0,0 +1,13 @@
+package business.impl;
+
+import business.usecase.CheckOutBookUseCase;
+
+public class CheckOutBookController implements CheckOutBookUseCase{
+
+	@Override
+	public void checkOutBook(String memberId, String bookId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
